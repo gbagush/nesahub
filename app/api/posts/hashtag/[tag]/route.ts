4733efe53f9d75ec08a799244d53f0b9ee235f03
Dziqha <lulussnbp01@gmail.com>
2025-05-14 21:36:55 +0700

    feat(posts): add route hashtag

---
+++ app/api/posts/hashtag/[tag]/route.ts
@@ -0,0 +1,43 @@
+import { db } from "@/lib/db";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(
+  _: NextRequest,
+  { params }: { params: Promise<{ tag: string }> }
+) {
+  try {
+    const hashtag = (await params).tag;
+
+    if (!hashtag) {
+      return NextResponse.json(
+        { message: "Hashtag parameter is required" },
+        { status: 400 }
+      );
+    }
+
+    const tagToSearch = hashtag.startsWith("#") ? hashtag : `#${hashtag}`;
+
+    const posts = await db.post.findMany({
+      where: {
+        postTags: {
+          some: {
+            hashtag: {
+              tag: tagToSearch,
+            },
+          },
+        },
+      },
+      include: {
+        author: true,
+      },
+    });
+
+    return NextResponse.json({ posts }, { status: 200 });
+  } catch (error) {
+    console.error("Error fetching posts by hashtag:", error);
+    return NextResponse.json(
+      { message: "Internal server error." },
+      { status: 500 }
+    );
+  }
+}
